32a88988f17f9cbaa95ca002af76ded9168f30d9
Pysis <Pysis868@Gmail.com>
2015-10-07 22:28:23 -0400

    Add Fish support
    Adds an option to instead add the convenience command to a Fish
    terminal configuration file.
    Also changed the way the instructions are detected to better avoid
    duplicate installations.

--- README.md
+++ README.md
@@ -7,32 +7,32 @@ A redistribution of the "ipa2say.rb" script developed by "OldPeculier" in a [Sup
 I take no credit for creating the content of the script, but merely want to make it more convenient to access.
 I will however, take credit for making the install script which adds the convenience command to your terminal emulator's login script :).
 
-## Installation Paths
+## Installation
 
-### Default
+ - First, download the project:
 
-Installs into `$HOME/.bashrc`:
-```bash
- mkdir "$HOME/bin";
- cd "$HOME/bin";
- git clone "https://github.com/Pysis868/IPA2Say";
- cd "IPA2SAY";
- chmod +x "install.sh";
- ./install.sh
-```
-
-### Alternative
-
-Installs into the file of your choice using `-c`, for example: `$HOME/.bash_profile`:
-```bash
- mkdir "$HOME/bin";
- cd "$HOME/bin";
- git clone "https://github.com/Pysis868/IPA2Say";
- cd "IPA2SAY";
- chmod +x "install.sh";
- ./install.sh -c "$HOME/.bash_profile";
-```
+ ```bash
+  mkdir "$HOME/bin";
+  cd "$HOME/bin";
+  git clone "https://github.com/Pysis868/IPA2Say";
+  cd "IPA2Say";
+  chmod +x "install.sh";
+ ```
+ - Then run one of the following commands based on your preference/environment:
+  - Default Bash Installation; installs into `$HOME/.bashrc` by default:
+   ```bash
+     ./install.sh
+   ```
+  - Alternative Bash Installation; installs into the file of your choice using the `-c` switch, for example: `$HOME/.bash_profile`:
+   ```bash
+     ./install.sh -c "$HOME/.bash_profile";
+   ```
+  - Fish Installation; installs into `$HOME/.config/fish/config.fish` by default, using the `-f` switch:
+   ```bash
+     ./install.sh -f
+   ```
+     Note: You may also use the `-c` switch in this case.
 
 ## Usage
 
-`sayIPA "ˌɪntərˈnæʃənəl fəˈnɛtɪk ˈælfəˌbɛt fəˈrɛvər"`
+Example: `sayIPA "ˌɪntərˈnæʃənəl fəˈnɛtɪk ˈælfəˌbɛt fəˈrɛvər"`
--- install.sh
+++ install.sh
@@ -3,15 +3,28 @@ SDIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )";
 
 [[ -z "$__DEBUG__" ]] && __DEBUG__="false";
 
+debugPrint() {
+  [[ "$__DEBUG__" == "true" ]] && echo -e "$*";
+}
+
+defaultConfigurationFilePathString="\$HOME/.bashrc";
+defaultConfigurationFilePath="$(eval "echo \"$defaultConfigurationFilePathString\"")";
+defaultFishConfigurationFilePathString="\$HOME/.config/fish/config.fish";
+defaultFishConfigurationFilePath="$(eval "echo \"$defaultFishConfigurationFilePathString\"")";
+
 usage() {
-  echo -e "Usage: $(basename "$0") [-c <userConfigurationFilePath>] [-h]";
-  echo -e "\t-h - Displays help text for using this script, and then exits."
-  echo -e "\t-c userConfigurationFilePath - A string containing the expected output file path for the current user's terminal configuration file. If no file path is specified, a default of \"$HOME/.bashrc\" is used.  The file's existing contents will be preserved, with the contents added by this script only if they do not already exist.";
+  echo -e "Usage: $(basename "$0") [-h] [-c <userConfigurationFilePath>] [-f]";
+  echo -e "\t-h - Displays help text for using this script, and then exits.";
+  echo -e "\t-c userConfigurationFilePath - Accepts a string containing the expected output file path for the current user's terminal configuration file.";
+  echo -e "\t-f - Instead of generating Bash-supported instructions, generates instructions that install the convenience command for the Friendly Interactive SHell.  Changes the default terminal configuration file path to \"$defaultFishConfigurationFilePath\"";
+  echo -e "";
+  echo -e "By default, this script installs to the \"$defaultConfigurationFilePathString\" terminal login configuration file.";
+  echo -e "The file's existing contents will be preserved, with the contents added by this script only if they do not already exist.";
   
   exit 1;
 }
 
-while getopts ":c:h" option; do
+while getopts ":c:hf" option; do
   case "$option" in
     h)
       usage;
@@ -19,10 +32,14 @@ while getopts ":c:h" option; do
     c)
       userConfigurationFilePath="$OPTARG";
     ;;
+    f)
+      generateFishConfiguration="true";
+      userConfigurationFilePath="$defaultFishConfigurationFilePath";
+    ;;
     \?)
       echo "Invalid option: -$OPTARG" >&2;
       usage;
-      exit 1
+      exit 1;
     ;;
     :)
       echo "Option -$OPTARG requires an argument." >&2;
@@ -33,18 +50,35 @@ done
 
 shift $((OPTIND-1));
 
-[[ -z "$userConfigurationFilePath" ]] && userConfigurationFilePath="$HOME/.bashrc";
-commandString="alias sayIPA='echo \"\$1\" | ruby $SDIR/ipa2say.rb | say';";
+[[ -z "$userConfigurationFilePath" ]] && userConfigurationFilePath="$defaultConfigurationFilePath";
 
-[[ "$__DEBUG__" == "true" ]] && echo "SDIR: $SDIR";
-[[ "$__DEBUG__" == "true" ]] && echo "commandString: $commandString";
-[[ "$__DEBUG__" == "true" ]] && echo "userConfigurationFilePath: $userConfigurationFilePath";
+if [[ "$generateFishConfiguration" == "true" ]]; then
+  read -r -d '' commandString << EOI
+function sayIPA
+  echo "\$1" | ruby "$SDIR/ipa2say.rb" | say;
+end
+EOI
+  commandStringIdentifier="${commandString:0:15}";
+else
+  commandString="alias sayIPA='echo \"\$1\" | ruby \"$SDIR/ipa2say.rb\" | say';";
+  commandStringIdentifier="${commandString:0:13}";
+fi
+
+debugPrint "SDIR: $SDIR";
+debugPrint "defaultConfigurationFilePathString: $defaultConfigurationFilePathString";
+debugPrint "defaultConfigurationFilePath: $defaultConfigurationFilePath";
+debugPrint "defaultFishConfigurationFilePathString: $defaultFishConfigurationFilePathString";
+debugPrint "defaultFishConfigurationFilePath: $defaultFishConfigurationFilePath";
+debugPrint "userConfigurationFilePath: $userConfigurationFilePath";
+debugPrint "generateFishConfiguration: $generateFishConfiguration";
+debugPrint "commandString: $commandString";
+debugPrint "commandStringIdentifier: $commandStringIdentifier";
 
 if [[ ! -f "$userConfigurationFilePath" ]]; then
   touch "$userConfigurationFilePath";
 fi
 
-if grep -Fq "$commandString" "$userConfigurationFilePath"; then
+if grep -Fq "$commandStringIdentifier" "$userConfigurationFilePath"; then
   echo "Instruction found; exiting without changes...";
 else
   echo "Instruction not found; inserting instruction...";
